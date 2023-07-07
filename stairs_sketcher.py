63c0864798a498956110385f8cbb492be33302e3
Nikita Akimov <korchiy@yandex.ru>
2023-07-07 14:03:29 +0300

    update

--- stairs_sketcher.py
+++ stairs_sketcher.py
@@ -30,46 +30,77 @@ class StairsSketcher:
     @classmethod
     def create_stairs_line(cls, context):
         # Create stairs line from selected loop
-        print('create stairs line')
-        # # current mode
-        # mode = context.active_object.mode
-        # # switch to Edit mode
-        # if context.active_object.mode == 'OBJECT':
-        #     bpy.ops.object.mode_set(mode='EDIT')
-        # # switch to edges mode
-        # context.tool_settings.mesh_select_mode = (False, True, False)
-        # # switch to Object mode
-        # bpy.ops.object.mode_set(mode='OBJECT')
-        # # deselect all data
-        # cls._deselect_all(obj_data=context.object.data)
-        # # select UV-Seam edges and set crease by them
-        # bm = bmesh.new()
-        # bm.from_mesh(context.object.data)
-        # bm.edges.ensure_lookup_table()
-        # # select seam edges
-        # seam_edges = [e for e in bm.edges if e.seam]
-        # for edge in seam_edges:
-        #     edge.select = True
-        # # set crease
-        # crease_layer = bm.edges.layers.crease.verify()
-        # for edge in bm.edges:
-        #     if edge.select:
-        #         edge[crease_layer] = 1.0
-        #     else:
-        #         edge[crease_layer] = 0.0
-        # # save changed data to mesh
-        # bm.to_mesh(context.object.data)
-        # # return mode back
-        # bpy.ops.object.mode_set(mode=mode)
-    #
-    # @staticmethod
-    # def _deselect_all(obj_data):
-    #     for polygon in obj_data.polygons:
-    #         polygon.select = False
-    #     for edge in obj_data.edges:
-    #         edge.select = False
-    #     for vertex in obj_data.vertices:
-    #         vertex.select = False
+        src_obj = context.active_object
+        # switch to Object mode
+        bpy.ops.object.mode_set(mode='OBJECT')
+        # create new empty mesh
+        stairs_mesh = context.blend_data.meshes.new('stairs')
+        stairs_obj = context.blend_data.objects.new('stairs', stairs_mesh)
+        stairs_obj.location = src_obj.location
+        context.scene.objects.link(stairs_obj)
+        # get data loop from source mesh
+        bm = bmesh.new()
+        bm.from_mesh(context.object.data)
+        bm.verts.ensure_lookup_table()
+        bm.edges.ensure_lookup_table()
+        # remove not-selected vertices
+        [bm.verts.remove(vert) for vert in bm.verts if not vert.select]
+        # source vertices
+        src_vertices = [vert for vert in bm.verts]
+        if src_vertices:
+            top_vert = max([vert for vert in src_vertices], key=lambda vert: vert.co.z)
+            first_vert = next((vert for vert in src_vertices
+                               if len(vert.link_edges) == 1 and vert != top_vert), None)
+            selection_loop_sorted = cls.vertices_loop_sorted(
+                bmesh_vertices_list=src_vertices,
+                bmesh_first_vertex=first_vert
+            )
+            stairs_height = top_vert.co.z - first_vert.co.z
+            stair_height = stairs_height / len(src_vertices)
+            # create stairs by sorted vertices
+            prev_stair_vert = None
+            for _i, vertex in enumerate(selection_loop_sorted):
+                co_l = vertex.co.copy()     # lower stair point
+                co_l.z = first_vert.co.z + stair_height * _i
+                vert_l = bm.verts.new(co_l)
+                co_h = vertex.co.copy()     # higher stair point
+                co_h.z = first_vert.co.z + stair_height * (_i + 1)
+                vert_h = bm.verts.new(co_h)
+                bm.edges.new((vert_l, vert_h))
+                if prev_stair_vert:
+                    bm.edges.new((prev_stair_vert, vert_l))
+                prev_stair_vert = vert_h
+            # remove src vertices
+            [bm.verts.remove(vert) for vert in src_vertices]
+            # select all
+            [vert.select_set(True) for vert in bm.verts if not vert.select]
+        # save changed data to stairs mesh
+        bm.to_mesh(stairs_mesh)
+        bm.free()
+        context.scene.objects.active = stairs_obj
+        src_obj.select = False
+        stairs_obj.select = True
+
+    @staticmethod
+    def vertices_loop_sorted(bmesh_vertices_list, bmesh_first_vertex):
+        # return list with vertices sorted by following each other in the loop
+        vertices_sorted = []
+        if bmesh_vertices_list and bmesh_first_vertex:
+            vertex = bmesh_first_vertex
+            _l = len(bmesh_vertices_list)
+            i = 0
+            while vertex is not None:
+                vertices_sorted.append(vertex)
+                edge = next((_edge for _edge in vertex.link_edges
+                             if _edge.other_vert(vertex) not in vertices_sorted), None)
+                vertex = edge.other_vert(vertex) if edge else None
+                # alarm break
+                i += 1
+                if i > _l:
+                    print('_points_sorted() err exit')
+                    break
+        # return sorted sequence
+        return vertices_sorted
 
 
 # OPERATORS
